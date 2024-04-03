539d8c1abec1a75e389acaa833be8485a19cb694
charan2334 <165793885+charan2334@users.noreply.github.com>
2024-04-03 12:09:13 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -3,7 +3,7 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class Example {
 
