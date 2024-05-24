d0c65db42cc3b8b0586939e73aa4b59a6dd24ed8
RutujaPandit123 <rutuja@synergytechs.net>
2024-05-24 12:27:21 +0530

    updated code

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -3,13 +3,13 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class Example {
 
     @RequestMapping("/")
-    public String index() {
-        return "Testing Springboot app pineapple - try 1";
+    String home() {
+        return "Testing";
     }
 
     public static void main(String[] args) throws Exception {
